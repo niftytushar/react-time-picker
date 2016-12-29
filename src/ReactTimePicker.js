e8141c360455d743ed5bec5b54ec93d9f51f8204
Tushar Agarwal <niftytushar@gmail.com>
2016-12-30 00:37:37 +0530

    implement hours and minutes dropdown add less stylesheet

--- src/ReactTimePicker.js
+++ src/ReactTimePicker.js
@@ -1,9 +1,59 @@
-var React = require('react');
+import React from 'react';
 
-var ReactTimePicker = React.createClass({
+export default class ReactTimePicker extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hour: 0,
+      minute: 0,
+    };
+
+    this.onHourSelect = this.onHourSelect.bind(this);
+  }
+
+  onHourSelect(event) {
+    event.stopPropagation();
+
+    this.setState({
+      hour: event.target.dataset.value,
+    });
+  }
+
+  onMinuteSelect(event) {
+    event.stopPropagation();
+
+    this.setState({
+      minute: event.target.dataset.value,
+    });
+  }
+
 	render () {
-		return <div>react-time-picker</div>;
-	}
-});
+    const hours = Array.from(new Array(12), (val, index) => index + 1);
+    const minutes = Array.from(new Array(60), (val, index) => index + 1);
 
-export default ReactTimePicker;
+		return <div className="rtp">
+      <input type="text" />
+      <div className="rtp-dropdown">
+        <ul onClick={this.onHourSelect}>
+          {
+            hours.map((hour) => {
+              return <li key={hour} data-value={hour}>{hour}</li>
+            })
+          }
+        </ul>
+        <ul>
+          {
+            minutes.map((minute) => {
+              return <li key={minute} data-value={minute}>{minute}</li>
+            })
+          }
+        </ul>
+        <ul>
+          <li>AM</li>
+          <li>PM</li>
+        </ul>
+      </div>
+    </div>;
+	}
+};
